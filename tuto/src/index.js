c4a216cac0578ef76d61f36d0f53c0a87257a3e0
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-02 18:35:47 +0100

    learning react fragment

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -5,7 +5,15 @@ import ReactDOM from 'react-dom/client'
 //     return <h2>My first Component</h2>
 // }
 function Greetings(){
-    return React.createElement('h2',{}, "Hello world")
+    return <React.Fragment>
+        <div>
+            <h2>Hello to the world</h2>
+            <ul>
+                <li><a href="#">Welcome</a></li>
+            </ul>
+        </div>
+        <h2>Hello again</h2>
+    </React.Fragment>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
