27e118524f056980dcccd6e67bf54ecc43daf56e
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-02 18:25:23 +0100

    createElement functionÃ

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// function Greetings(){
+//     return <h2>My first Component</h2>
+// }
 function Greetings(){
-    return <h2>My first Component</h2>
+    return React.createElement('h2',{}, "Hello world")
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<Greetings />)
-export default Greetings
+// export default Greetings
