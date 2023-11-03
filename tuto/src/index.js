2d2aa24b5d733e5708a688a1c158063ecad60f80
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-03 11:53:24 +0100

    setting up props

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -2,22 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+const title = "Friends, Lovers, and the Big Terrible Thing"
+const author = "Matthew Perry"
+const image = "./images/book-image.jpg"
 const BookList = () => {
+
     return <section className="bookList">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book author={author} title={title} image={image}/>
+        <Book author={author} title={title} image={image}/>
     </section>
 }
-
-const Book = ()=>{
-    const title = "Friends, Lovers, and the Big Terrible Thing"
-    const author = "Matthew Perry"
+const Book = (props)=>{
     return <article className="book">
-        <img src='./images/book-image.jpg'  alt="bookImage"/>
-        <h2>{title}</h2>
-        <h4>{author}</h4>
+        <img src={props.image}  alt={props.title}/>
+        <h2>{props.title}</h2>
+        <h4>{props.author}</h4>
     </article>
 }
 
