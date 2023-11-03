ac87b3a027f889c2162342b220480a25bb4e5d94
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-03 15:24:21 +0100

    working with map to render objects in react

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -2,32 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const firstBook = {
+
+const books = [{
+    id:1,
     title : "Friends, Lovers, and the Big Terrible Thing",
     author : "Matthew Perry",
     image :"./images/book-image.jpg"
-}
-const secondBook = {
+}, {
+    id:2,
     title : "The Woman in Me",
     author : "Britney-Spears",
     image :"./images/2nd-book-image.jpg"
-}
-const thirdBook = {
+},
+ {
+     id:3,
     title : "Iron Flame (The Empyrean, 2) ",
     author : " Rebecca Yarros",
     image :"./images/3rd-book-image.jpg"
-}
+}]
+
 
 const BookList = () => {
 
-    return <section className="bookList">
-        <Book author={firstBook.author} title={firstBook.title} image={firstBook.image}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate eos est eveniet fuga ipsam molestiae molestias mollitia officia perferendis ratione reiciendis repellat similique, sit vitae! Iure omnis provident soluta.</p>
-            <button>click me</button>
-        </Book>
-        <Book author={secondBook.author} title={secondBook.title} image={secondBook.image}/>
-        <Book author={thirdBook.author} title={thirdBook.title} image={thirdBook.image}/>
-    </section>
+    return <section className="bookList">{books.map((book)=>{
+        return (
+            <Book title={book.title} image={book.image} author={book.author} key={book.id}/>
+        )
+    })}</section>
 }
 const Book = (props)=>{
     const {image, title,author, children} = props
