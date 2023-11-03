d30bd6cc99db83aab7e24849d38a5469122cee18
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-03 12:22:50 +0100

    working with props object

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -2,21 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const title = "Friends, Lovers, and the Big Terrible Thing"
-const author = "Matthew Perry"
-const image = "./images/book-image.jpg"
+const firstBook = {
+    title : "Friends, Lovers, and the Big Terrible Thing",
+    author : "Matthew Perry",
+    image :"./images/book-image.jpg"
+}
+const secondBook = {
+    title : "The Woman in Me",
+    author : "Britney-Spears",
+    image :"./images/2nd-book-image.jpg"
+}
+const thirdBook = {
+    title : "Iron Flame (The Empyrean, 2) ",
+    author : " Rebecca Yarros",
+    image :"./images/3rd-book-image.jpg"
+}
+
 const BookList = () => {
 
     return <section className="bookList">
-        <Book author={author} title={title} image={image}/>
-        <Book author={author} title={title} image={image}/>
+        <Book author={firstBook.author} title={firstBook.title} image={firstBook.image}/>
+        <Book author={secondBook.author} title={secondBook.title} image={secondBook.image}/>
+        <Book author={thirdBook.author} title={thirdBook.title} image={thirdBook.image}/>
     </section>
 }
 const Book = (props)=>{
+    const {image, title,author} = props
     return <article className="book">
-        <img src={props.image}  alt={props.title}/>
-        <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+        <img src={image}  alt={title}/>
+        <h2>{title}</h2>
+        <h4>{author}</h4>
     </article>
 }
 
