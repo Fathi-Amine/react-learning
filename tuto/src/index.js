3339f6a5f30cc2b14002e5996bb3ba186828e067
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2023-11-03 16:14:05 +0100

    Learning event basics

--- tuto/src/index.js
+++ tuto/src/index.js
@@ -24,11 +24,29 @@ const books = [{
 
 const BookList = () => {
 
-    return <section className="bookList">{books.map((book)=>{
-        return (
-            <Book title={book.title} image={book.image} author={book.author} key={book.id}/>
-        )
+    return <section className="bookList">
+        <EventExamples />
+        {books.map((book)=>{
+            return <Book title={book.title} image={book.image} author={book.author} key={book.id}/>
+
     })}</section>
+}
+
+const EventExamples = ()=>{
+    const handleFormInput = ()=>{
+        console.log("handle form input")
+    }
+
+    const handleButtonClick = ()=>{
+        alert("Handle button click")
+    }
+    return <section>
+        <form>
+            <h2>Typical form</h2>
+            <input type="text" name="example" onChange={handleFormInput} style={{margin:'1rem 0'}}/>
+        </form>
+        <button onClick={handleButtonClick}>click me</button>
+    </section>
 }
 const Book = (props)=>{
     const {image, title,author, children} = props
